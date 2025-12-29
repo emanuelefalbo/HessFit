d3e4b8ebf5bcf07a88fd8006ae211994d42aa5d2
emanuele <piccolochimico93@gmail.com>
2025-12-29 15:51:10 +0100

    .toml update

--- hessfit/__init__.py
+++ hessfit/__init__.py
@@ -9,6 +9,7 @@
 
 from os.path import dirname, basename, isfile, join
 import glob
+from .hessfit import main
 
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
--- molecular-viewer-app/src/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-# FILE: /molecular-viewer-app/molecular-viewer-app/src/utils/__init__.py
-# This file is intentionally left blank.
